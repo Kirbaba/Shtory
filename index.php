edb7170db7a5400ff785a9e9ab657da2585c05da
Vladislav <v.kirbaba@gmail.com>
2016-03-01 10:57:41 +0300

    Fonts + head

--- index.php
+++ index.php
@@ -11,8 +11,50 @@
     <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
-
 <body>
+	<!-- open .home-screen -->
+	<div class="home-screen">
+		<!-- open .header -->
+		<header class="header">
+			<!-- open .container -->
+			<div class="container">
+				<!-- open .row -->
+				<div class="row">
+					<!-- open .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+						<!-- open .header__adress -->
+						<div class="header__adress">
+							<h4>ПРАКТИКА КОМФОРТА</h4>
+							<p>г. Москва м. Румянцево, <br />Бизнес-парк "Румянцево»</p>
+						</div>
+						<!-- close .header__adress -->
+					</div>
+					<!-- close .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+					<!-- open .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+						<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="" />
+					</div>
+					<!-- close .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+					<!-- open .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+						<!-- open .header__phone -->
+						<div class="header__phone">
+							<h3><small>Бесплатная и быстрая консультация</small>
+							+7 903 722 63 49</h3>
+							<a href="#">ЗАКАЗАТЬ ЗВОНОК</a>
+						</div>
+						<!-- close .header__phone -->
+					</div>
+					<!-- close .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+				</div>
+				<!-- close .row -->
+			</div>
+			<!-- close .container -->
+		</header>
+		<!-- close .header -->
+		<home-slider></home-slider>
+	</div>
+	<!-- close .home-screen -->
     
 <?php wp_footer(); ?>
 </body>
