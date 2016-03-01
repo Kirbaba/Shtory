794770e5a4e3a1d8f6389bfb5e34d3ba6e367af0
Vladislav <v.kirbaba@gmail.com>
2016-03-01 14:12:32 +0300

    Slider

--- index.php
+++ index.php
@@ -20,39 +20,67 @@
 			<div class="container">
 				<!-- open .row -->
 				<div class="row">
-					<!-- open .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
-					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-						<!-- open .header__adress -->
-						<div class="header__adress">
-							<h4>ПРАКТИКА КОМФОРТА</h4>
-							<p>г. Москва м. Румянцево, <br />Бизнес-парк "Румянцево»</p>
+					<!-- open .col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-12 col-xs-12 -->
+					<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
+						<!-- open .row -->
+						<div class="row">
+							<!-- open .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+							<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+								<!-- open .header__adress -->
+								<div class="header__adress">
+									<h4>ПРАКТИКА КОМФОРТА</h4>
+									<p>г. Москва м. Румянцево, <br />Бизнес-парк "Румянцево»</p>
+								</div>
+								<!-- close .header__adress -->
+							</div>
+							<!-- close .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+							<!-- open .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+							<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+								<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="" />
+							</div>
+							<!-- close .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+							<!-- open .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+							<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+								<!-- open .header__phone -->
+								<div class="header__phone">
+									<h3><small>Бесплатная и быстрая консультация</small>
+									+7 903 722 63 49</h3>
+									<a href="#">ЗАКАЗАТЬ ЗВОНОК</a>
+								</div>
+								<!-- close .header__phone -->
+							</div>
+							<!-- close .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
 						</div>
-						<!-- close .header__adress -->
+						<!-- close .row -->
 					</div>
-					<!-- close .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
-					<!-- open .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
-					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-						<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="" />
-					</div>
-					<!-- close .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
-					<!-- open .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
-					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-						<!-- open .header__phone -->
-						<div class="header__phone">
-							<h3><small>Бесплатная и быстрая консультация</small>
-							+7 903 722 63 49</h3>
-							<a href="#">ЗАКАЗАТЬ ЗВОНОК</a>
-						</div>
-						<!-- close .header__phone -->
-					</div>
-					<!-- close .col-lg-4 col-md-4 col-sm-4 col-xs-12 -->
+					<!-- close .col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-12 col-xs-12 -->
+					
 				</div>
 				<!-- close .row -->
 			</div>
 			<!-- close .container -->
 		</header>
 		<!-- close .header -->
-		<home-slider></home-slider>
+		<!-- open .home-slider -->
+		<div class="home-slider">
+			<!-- open .fotorama -->
+			<div class="fotorama" data-width="100%" data-radio="1925/460" data-fit="cover" data-autoplay="true" data-loop="true" data-arrows="false">
+				<!-- open .home-slider__item -->
+				<div data-img="<?php bloginfo('template_directory'); ?>/img/fon.jpg">
+				hello</div>
+				<!-- close .home-slider__item -->
+				<!-- open .home-slider__item -->
+				<div data-img="<?php bloginfo('template_directory'); ?>/img/fon.jpg">
+				hello</div>
+				<!-- close .home-slider__item -->
+				<!-- open .home-slider__item -->
+				<div data-img="<?php bloginfo('template_directory'); ?>/img/fon.jpg">
+				hello</div>
+				<!-- close .home-slider__item -->
+			</div>
+			<!-- close .fotorama -->
+		</div>
+		<!-- close .home-slider -->
 	</div>
 	<!-- close .home-screen -->
     
